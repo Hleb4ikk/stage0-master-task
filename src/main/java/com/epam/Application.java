e29c6ebc7a89b6cecf80d163a4e05b2337d657e9
Hleb4ikk <hlebor2006@mail.ru>
2024-06-29 19:27:14 +0300

    Conditions task 4 completed

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -4,6 +4,7 @@ import com.epam.OOP.Animal;
 import com.epam.OOP.Bird;
 import com.epam.OOP.Dog;
 import com.epam.conditions.DaysInMonth;
+import com.epam.conditions.IntegerDivider;
 import com.epam.conditions.SeasonDeterminer;
 import com.epam.conditions.TriangleSidesValidator;
 import com.epam.langSyntax.DigitsSumCalculator;
@@ -26,6 +27,7 @@ public class Application {
         SeasonDeterminer.tellTheSeason(15);
         TriangleSidesValidator.validate(1,3,3);
         DaysInMonth.printDays(2011, 2);
+        IntegerDivider.printCompletelyDivided(20, 3);
     }
 
 
